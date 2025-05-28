428cf1f7d13e2261c2f183efe4ca9e820bd8d94a
SasraMochi <sasra0873@gmail.com>
2025-05-29 08:35:50 +0900

    Jsonからのツリー構築に対応

--- Src/Math/BehaviourTree/BehaviourTreeBulider.cpp
+++ Src/Math/BehaviourTree/BehaviourTreeBulider.cpp
@@ -22,16 +22,6 @@ using json = nlohmann::json;
 
 INode* BehaviourTreeBuilder::BuildAttackerTree(BlackBoard* blackboard)
 {
-	// 直書き版
-#if 0
-	auto chase_inverter = new Inverter(blackboard, new ChasePlayerLeaf(blackboard));
-
-	auto root_sequence = new Sequence(blackboard);
-	root_sequence->add_node(new CheckNearPlayer(blackboard, new EscapeFromPlayerLeaf(blackboard), new ChasePlayerLeaf(blackboard), 500.0f));
-	return root_sequence;
-#endif
-
-#if 1
 	// JSONファイルを読み込む
 	std::ifstream file("behavior_tree.json");
 	if (!file.is_open())
@@ -52,89 +42,96 @@ INode* BehaviourTreeBuilder::BuildAttackerTree(BlackBoard* blackboard)
 
 	// 再帰的にノードを構築するラムダ関数
 	std::function<INode* (int)> buildNode = [&](int nodeId) -> INode*
+	{
+		if (nodeMap.find(nodeId) == nodeMap.end())
 		{
-			if (nodeMap.find(nodeId) == nodeMap.end())
-			{
-				throw std::runtime_error("Node id not found in JSON: " + std::to_string(nodeId));
-			}
-			auto nodeJson = nodeMap[nodeId];
-			std::string name = nodeJson["name"].get<std::string>();
+			throw std::runtime_error("Node id not found in JSON: " + std::to_string(nodeId));
+		}
+		auto nodeJson = nodeMap[nodeId];
+		std::string name = nodeJson["name"].get<std::string>();
 
-			if (name == "Sequence")
+
+		// --- Compositeノード ---
+		if (name == "Sequence")
+		{
+			Sequence* seq = new Sequence(blackboard);
+			for (auto childId : nodeJson["children"])
 			{
-				Sequence* seq = new Sequence(blackboard);
-				for (auto childId : nodeJson["children"])
-				{
-					seq->add_node(buildNode(childId.get<int>()));
-				}
-				return seq;
+				seq->add_node(buildNode(childId.get<int>()));
 			}
-			else if (name == "Inverter")
+			return seq;
+		}
+		else if (name == "Selector")
+		{
+			Selector* selector = new Selector(blackboard);
+			for (auto childId : nodeJson["children"])
 			{
-				// Inverterは子を1つ持つと仮定
-				int childId = nodeJson["children"][0].get<int>();
-				return new Inverter(blackboard, buildNode(childId));
+				selector->add_node(buildNode(childId.get<int>()));
 			}
-			else if (name == "CheckNearPlayer")
-			{
-				// true_child と false_child を使って分岐する
-				int trueChildId = nodeJson["true_child"].get<int>();
-				int falseChildId = nodeJson["false_child"].get<int>();
-				float limitDistance = nodeJson["limit_distance"].get<float>();
-				return new CheckNearPlayer(blackboard, buildNode(trueChildId), buildNode(falseChildId), limitDistance);
-			}
-			else if (name == "CircleAttackLeaf")
-			{
-				return new CircleAttackLeaf(blackboard);
-			}
-			else if (name == "EscapeFromPlayerLeaf")
-			{
-				return new EscapeFromPlayerLeaf(blackboard);
-			}
-			else if (name == "Selector")
-			{
-				Selector* selector = new Selector(blackboard);
-				for (auto childId : nodeJson["children"])
-				{
-					selector->add_node(buildNode(childId.get<int>()));
-				}
-				return selector;
-			}
-			else if (name == "CheckFarPlayer")
-			{
-				int trueChildId = nodeJson["true_child"].get<int>();
-				int falseChildId = nodeJson["false_child"].get<int>();
-				float limitDistance = nodeJson["limit_distance"].get<float>();
-				return new CheckFarPlayer(blackboard, buildNode(trueChildId), buildNode(falseChildId), limitDistance);
-			}
-			else if (name == "DebugDrawLeaf")
-			{
-				int text = nodeJson["text"].get<int>();
-				return new DebugDrawLeaf(blackboard, text);
-			}
-			else if (name == "AlwaysFailLeaf")
-			{
-				return new AlwaysFailLeaf(blackboard);
-			}
-			else if (name == "WaitLeaf")
-			{
-				float waitTime = nodeJson["wait_time"].get<float>();
-				return new WaitLeaf(blackboard, waitTime);
-			}
-			else if (name == "AlwaysSuccessLeaf")
-			{
-				return new AlwaysSuccessLeaf(blackboard);
-			}
-			else if (name == "ChasePlayerLeaf")
-			{
-				return new ChasePlayerLeaf(blackboard);
-			}
-			// 他のノードについても必要に応じて実装を追加する
-			else
-			{
-				throw std::runtime_error("Unknown node type: " + name);
-			}
-		};
+			return selector;
+		}
+
+		// --- Decoratorノード ---
+		else if (name == "Inverter")
+		{
+			int childId = nodeJson["children"][0].get<int>();
+			return new Inverter(blackboard, buildNode(childId));
+		}
+
+		// --- Branchノード ---
+		else if (name == "CheckNearPlayer")
+		{
+			int trueChildId = nodeJson["true_child"].get<int>();
+			int falseChildId = nodeJson["false_child"].get<int>();
+			float limitDistance = nodeJson["limit_distance"].get<float>();
+			return new CheckNearPlayer(blackboard, buildNode(trueChildId), buildNode(falseChildId), limitDistance);
+		}
+		else if (name == "CheckFarPlayer")
+		{
+			int trueChildId = nodeJson["true_child"].get<int>();
+			int falseChildId = nodeJson["false_child"].get<int>();
+			float limitDistance = nodeJson["limit_distance"].get<float>();
+			return new CheckFarPlayer(blackboard, buildNode(trueChildId), buildNode(falseChildId), limitDistance);
+		}
+
+		// --- Leafノード ---
+		else if (name == "WaitLeaf")
+		{
+			float waitTime = nodeJson["wait_time"].get<float>();
+			return new WaitLeaf(blackboard, waitTime);
+		}
+		else if (name == "ChasePlayerLeaf")
+		{
+			return new ChasePlayerLeaf(blackboard);
+		}
+		else if (name == "EscapeFromPlayerLeaf")
+		{
+			return new EscapeFromPlayerLeaf(blackboard);
+		}
+		else if (name == "CircleAttackLeaf")
+		{
+			return new CircleAttackLeaf(blackboard);
+		}
+		else if (name == "AlwaysSuccessLeaf")
+		{
+			return new AlwaysSuccessLeaf(blackboard);
+		}
+		else if (name == "AlwaysFailLeaf")
+		{
+			return new AlwaysFailLeaf(blackboard);
+		}
+		else if (name == "DebugDrawLeaf")
+		{
+			int text = nodeJson["text"].get<int>();
+			return new DebugDrawLeaf(blackboard, text);
+		}
+
+		// --- 未知のノード ---
+		else
+		{
+			throw std::runtime_error("Unknown node type: " + name);
+		}
+	};
 
 	// ルートノードを探す (parentが-1のノード)
 	int rootId = -1;
@@ -152,5 +149,4 @@ INode* BehaviourTreeBuilder::BuildAttackerTree(BlackBoard* blackboard)
 	}
 
 	return buildNode(rootId);
-#endif
 }
